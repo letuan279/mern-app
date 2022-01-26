0ea6908fd1dab777b8dafe15b2963323189fa19d
tuanle <latuan388@gmail.com>
2022-01-26 11:22:03 +0700

    Donegit push -u origin main

--- client/src/contexts/constants.js
+++ client/src/contexts/constants.js
@@ -1,2 +1,2 @@
-export const apiURL = process.env.NODE_ENV !== 'production' ? 'https://sleepy-everglades-72104.herokuapp.com/api' : "something"
+export const apiURL = process.env.NODE_ENV !== 'production' ? 'http://localhost:5000/api' : "https://sleepy-everglades-72104.herokuapp.com/api"
 export const LOCAL_STORAGE_TOKEN_NAME = 'abcxyz'
